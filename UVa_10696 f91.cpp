6e297125ddfc6b3c3c26b7d0eaf512df20565644
Md. Abdur Rouf Nirob <arnirob741@gmail.com>
2018-05-17 12:07:09 +0600

    Add files via upload

---
+++ UVa_10696 f91.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    while(scanf("%d",&n),n)
+    {
+        if(n<=100) printf("f91(%d) = 91\n",n);
+        else printf("f91(%d) = %d\n",n,n-10);
+    }
+    return 0;
+}
